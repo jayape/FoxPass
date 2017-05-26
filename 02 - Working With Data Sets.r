1304058d412a6943244e7987787d33e368ffceef
jayape <jpertell@gmail.com>
2017-05-26 05:41:38 -0500

    Updated data

--- 02 - Working With Data Sets.r
+++ 02 - Working With Data Sets.r
@@ -5,7 +5,7 @@ mydata <- iris
 head(mydata)
 
 # Read data from file
-myCSV <- read.csv("./Data/CHI2017.csv", header = TRUE)
+myCSV <- read.csv("./Data/Chicago2016.csv", header = TRUE)
 head(myCSV)
 
 # Read data from a web page
@@ -23,6 +23,7 @@ write.csv(mystandings1, "./Data/NHL2017.csv", row.names = FALSE)
 
 # Connect to a SQL database
 library(RODBC)
+library(ggplot2)
 library(ggmap)
 
 myConn <- odbcDriverConnect("driver={SQL Server};
@@ -32,7 +33,7 @@ myConn <- odbcDriverConnect("driver={SQL Server};
 
 
 userData <- sqlFetch(myConn, "Locations")
-#close(myConn)
+close(myConn)
 
 map <- get_map("United State", scale = 2, zoom = 4)
 
@@ -40,5 +41,75 @@ ggmap(map) +
   geom_point(aes(x = lon, y = lat), data = userData, alpha = .5)
 
 
-sqlSave(myConn, dat = myCSV, tablename = 'Chicago_2017', safer = TRUE, rownames = FALSE)
+url <- "http://www.hockey-reference.com/teams/CHI/2017_games.html"
+
+tables <- readHTMLTable(url, stringsAsFactors = FALSE)
+temp <- tables$games
+names(temp) <- c("GP", "Date", "Time", "Loc", "Opponent", "GF", "GA", "Result", "OT", "W", "L", "OL", "Streak", "Blank1", 
+                 "tS", "tPIM", "tPPG", "tPPO", "tSHG", "Blank2", "oS","oPIM", "oPPG", "oPPO", "oSHG", "Att", "LOG", "Notes")
+temp <- subset(temp, temp$GP  != "GP" & temp$Date != "Cumulative" & temp$Date != "Team" & temp$Result != "")
+temp <- temp[, c(1, 2, 4:9, 15:19, 21:27)]
+
+temp$GP <- as.integer(temp$GP)
+temp$Date <- as.Date(temp$Date)
+temp$Loc <- as.character(temp$Loc)
+temp$Loc[temp$Loc == "@"] <- "A"
+temp$Loc[temp$Loc == ""] <- "H"
+temp$Loc <- as.factor(temp$Loc)
+temp$GF <- as.integer(temp$GF)
+temp$GA <- as.integer(temp$GA)
+temp$Result <- as.factor(temp$Result)
+temp$OT[temp$OT == ""] <- "REG"
+temp$OT <- as.factor(temp$OT)
+temp$tS <- as.integer(temp$tS)
+temp$tPIM <- as.integer(temp$tPIM)
+temp$tPPG <- as.integer(temp$tPPG)
+temp$tPPO <- as.integer(temp$tPPO)
+temp$tSHG <- as.integer(temp$tSHG)
+temp$oS <- as.integer(temp$oS)
+temp$oPIM <- as.integer(temp$oPIM)
+temp$oPPG <- as.integer(temp$oPPG)
+temp$oPPO <- as.integer(temp$oPPO)
+temp$oSHG <- as.integer(temp$oSHG)
+temp$Att <- gsub("[[:punct:]]","", temp[, 19])
+temp$Att <- as.integer(temp$Att)
+temp$diff <- temp$GF - temp$GA
+
+chi2017 <- temp
+head(chi2017)
+
+mean(chi2017$GF)
+min(chi2017$GF)
+max(chi2017$GF)
+median(chi2017$GF)
+summary(chi2017$GF)
+summary(chi2017)
+aggregate(GF ~ Result, chi2017, mean)
+aggregate(cbind(GF, GA, tS, oS, tPIM, oPIM, tPPG, oPPG) ~ Result + Loc, chi2017, mean)
+aggregate(cbind(GF, GA, tS, oS, tPIM, oPIM, tPPG, oPPG) ~ Opponent, chi2017, mean)
+var(chi2017$GF)
+sd(chi2017$GF)
+cor(chi2017$tS, chi2017$GF)
+lm(GF ~ GA, chi2017)
+
+ggplot(chi2017, aes(x = GF, y = GA)) +
+  geom_point() +
+  geom_smooth(method = "lm") + labs(x = "Goals For", y = "Goals Against")
+
+par(mfrow=c(2,2))
+hist(chi2017$GF[chi2017$Loc == "H"], freq=F, col = "lightblue", main = "Home Goals Scored", xlab = "Number of Goals")
+curve(dnorm(x, mean(chi2017$GF[chi2017$Loc == "H"]), sd(chi2017$GF[chi2017$Loc == "H"])), add=T)
+hist(chi2017$GA[chi2017$Loc == "H"], freq=F, col = "orangered", main = "Home Goals Allowed", xlab = "Number of Goals")
+curve(dnorm(x, mean(chi2017$GA[chi2017$Loc == "H"]), sd(chi2017$GA[chi2017$Loc == "H"])), add=T)
+hist(chi2017$GF[chi2017$Loc == "A"], freq=F, col = "lightblue", main = "Away Goals Scored", xlab = "Number of Goals")
+curve(dnorm(x, mean(chi2017$GF[chi2017$Loc == "A"]), sd(chi2017$GF[chi2017$Loc == "A"])), add=T)
+hist(chi2017$GA[chi2017$Loc == "A"], freq=F, col = "orangered", main = "Away Goals Allowed", xlab = "Number of Goals")
+curve(dnorm(x, mean(chi2017$GA[chi2017$Loc == "A"]), sd(chi2017$GA[chi2017$Loc == "A"])), add=T)
+
+myConn <- odbcDriverConnect("driver={SQL Server};
+                            server=(local);
+                            database=DemoDB;
+                            Trusted_Connection=yes")
+
+sqlSave(myConn, dat = chi2017, tablename = 'Chicago_2017', safer = FALSE, rownames = FALSE, verbose = TRUE)
 close(myConn)
